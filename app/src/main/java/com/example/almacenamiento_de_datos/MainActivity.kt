bc34e971036ff5be6dbfe72c40bf16591553fc6e
Maite Martinez <salomeydavid507@gmail.com>
2023-03-23 18:01:51 -0500

    Actualizando archivos

--- app/src/main/java/com/example/almacenamiento_de_datos/MainActivity.kt
+++ app/src/main/java/com/example/almacenamiento_de_datos/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.almacenamiento_de_datos
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.widget.Button
 import android.widget.EditText
@@ -11,27 +12,14 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    var campoUsuario:EditText?=null
-    var campoPass:EditText?=null
-    var txtUsuario:TextView?=null
-    var txtPass:TextView?=null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        iniciarComponentes()
-        cargarDatos()
-
-    }
+    var campoUsuario: EditText? = null
+    var campoPass: EditText? = null
+    var txtUsuario: TextView? = null
+    var txtPass: TextView? = null
 
     private fun iniciarComponentes() {
-
-        var btnGuardar:Button=findViewById(R.id.btnGuardar)
-        btnGuardar.setOnClickListener { guardarDatos() }
-
         var btnCargar:Button=findViewById(R.id.btnCargar)
-        btnCargar.setOnClickListener { cargarDatos() }
+        btnCargar.setOnClickListener { validarDatos() }
 
         campoUsuario=findViewById(R.id.campoUser)
         campoPass=findViewById(R.id.campoPass)
@@ -40,33 +28,46 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun guardarDatos() {
-
-        var preferences:SharedPreferences=getSharedPreferences("credenciales",Context.MODE_PRIVATE)
-
-        var usuario= campoUsuario?.text.toString()
-        var pass= campoPass?.text.toString()
-
-        var editor:SharedPreferences.Editor=preferences.edit()
-        editor.putString("user",usuario)
-        editor.putString("pass",pass)
-
-        txtUsuario?.text=usuario
-        txtPass?.text=pass
-
-        editor.commit()
-
-        Toast.makeText(this,"Se han registrado los datos",Toast.LENGTH_SHORT).show()
-    }
-
-    private fun cargarDatos() {
-        var preferences:SharedPreferences=getSharedPreferences("credenciales",Context.MODE_PRIVATE)
-
-        var user: String? =preferences.getString("user","No existe la información")
-        var pass: String? =preferences.getString("pass","No existe la información")
-
-        txtUsuario?.text=user
-        txtPass?.text=pass
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        iniciarComponentes()
+        cargarDatos()
 
     }
+    private fun validarDatos() {
+        val usuario = campoUsuario?.text.toString()
+        val pass = campoPass?.text.toString()
+
+        if (usuario.isNotEmpty() && pass.isNotEmpty()) {
+            cargarDatos(usuario, pass)
+        } else {
+            Toast.makeText(this, "Por favor, ingrese usuario y contraseña", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun cargarDatos(user: String, pass: String) {
+        var preferences: SharedPreferences = getSharedPreferences("credenciales", Context.MODE_PRIVATE)
+        var storedUser: String? =preferences.getString("user","No existe la información")
+        var storedPass: String? =preferences.getString("pass","No existe la información")
+
+        if (storedUser == user && storedPass == pass) {
+            val intent = Intent(this, transfer_activity::class.java)
+            val content: Bundle = Bundle()
+
+            content.putString("nameUser", user)
+            content.putString("passwordUser", pass)
+
+            intent.putExtras(content)
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+
+
+
+
 }
---
+++ app/src/main/java/com/example/almacenamiento_de_datos/transfer_activity.kt
@@ -0,0 +1,35 @@
+package com.example.almacenamiento_de_datos
+
+import android.graphics.Color
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class transfer_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transfer)
+        val findleMessage = findViewById<TextView>(R.id.txtMessage)
+        val nameUser: String? = intent.getStringExtra("nameUser")
+        val passwordUser: String? = intent.getStringExtra("passwordUser")
+
+        var data: Bundle? = this.intent.extras
+
+
+        if (nameUser == "" && passwordUser == "") {
+            findleMessage.text = "No existe la información correspondiente"
+        }
+        else if(data != null) {
+            findleMessage.text = "Usuario: ${data.getString("nameUser")} \nContraseña: ${data.getString("passwordUser")}"
+        }
+            val btnExit:Button = findViewById(R.id.txtSalir)
+            btnExit.setOnClickListener{onClick()}
+        }
+
+
+        private fun onClick() {
+            finish()
+        }
+    }
